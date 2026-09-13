573ba610b58b98760a38701a637f7400e5ded33f
Nishant <158288105+Nishant-talenx@users.noreply.github.com>
2026-09-13 09:28:59 +0530

    Update router.dart

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,23 +1,26 @@
-```dart
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:imsnsit/root_scaffold.dart';
 import 'package:imsnsit/screens/about_screen.dart';
 import 'package:imsnsit/screens/attendance/attandance_screen.dart';
+import 'package:imsnsit/screens/attendance/subject_attendance_screen.dart';
 import 'package:imsnsit/screens/authentication/login_screen.dart';
 import 'package:imsnsit/screens/authentication/manual_relogin.dart';
 import 'package:imsnsit/screens/initial_screen.dart';
 import 'package:imsnsit/screens/profile_screen.dart';
-import 'package:imsnsit/screens/attendance/subject_attendance_screen.dart';
 import 'package:imsnsit/widgets/update_dialog.dart';
 
 final _rootNavigatorKey = GlobalKey<NavigatorState>();
 
 final _shellNavigatorProfileKey =
-    GlobalKey<NavigatorState>(debugLabel: 'shellProfile');
+    GlobalKey<NavigatorState>(
+  debugLabel: 'shellProfile',
+);
 
 final _shellNavigatorAttendanceKey =
-    GlobalKey<NavigatorState>(debugLabel: 'shellAttendance');
+    GlobalKey<NavigatorState>(
+  debugLabel: 'shellAttendance',
+);
 
 class MyAppRouter {
   static GoRouter router = GoRouter(
@@ -31,34 +34,50 @@ class MyAppRouter {
           );
         },
         branches: [
+          // ATTENDANCE
           StatefulShellBranch(
-            navigatorKey: _shellNavigatorAttendanceKey,
+            navigatorKey:
+                _shellNavigatorAttendanceKey,
             routes: [
               GoRoute(
                 path: '/attendance/total',
-                pageBuilder: (context, state) => const MaterialPage(
+                pageBuilder:
+                    (context, state) =>
+                        const MaterialPage(
                   child: AttandanceScreen(),
                   maintainState: true,
                 ),
               ),
               GoRoute(
                 name: 'subject_attendance',
-                path: '/attendance/subject_wise/:subject/:subjectCode',
-                pageBuilder: (context, state) => MaterialPage(
+                path:
+                    '/attendance/subject_wise/:subject/:subjectCode',
+                pageBuilder:
+                    (context, state) =>
+                        MaterialPage(
                   child: SubjectAttandanceScreen(
-                    subject: state.pathParameters['subject']!,
-                    subjectCode: state.pathParameters['subjectCode']!,
+                    subject:
+                        state.pathParameters[
+                            'subject']!,
+                    subjectCode:
+                        state.pathParameters[
+                            'subjectCode']!,
                   ),
                 ),
               ),
             ],
           ),
+
+          // PROFILE
           StatefulShellBranch(
-            navigatorKey: _shellNavigatorProfileKey,
+            navigatorKey:
+                _shellNavigatorProfileKey,
             routes: [
               GoRoute(
                 path: '/profile_screen',
-                pageBuilder: (context, state) => const MaterialPage(
+                pageBuilder:
+                    (context, state) =>
+                        const MaterialPage(
                   child: ProfileScreen(),
                 ),
               ),
@@ -66,33 +85,53 @@ class MyAppRouter {
           ),
         ],
       ),
+
+      // LOGIN
       GoRoute(
         path: '/authentication/login_screen',
-        pageBuilder: (context, state) => const MaterialPage(
+        pageBuilder:
+            (context, state) =>
+                const MaterialPage(
           child: LoginScreen(),
         ),
       ),
+
+      // MANUAL LOGIN
       GoRoute(
         path: '/authentication/manual_login',
-        pageBuilder: (context, state) => const MaterialPage(
+        pageBuilder:
+            (context, state) =>
+                const MaterialPage(
           child: ManualRelogin(),
         ),
       ),
+
+      // ABOUT
       GoRoute(
         path: '/about_screen',
-        pageBuilder: (context, state) => const MaterialPage(
+        pageBuilder:
+            (context, state) =>
+                const MaterialPage(
           child: AboutScreen(),
         ),
       ),
+
+      // INITIAL SCREEN
       GoRoute(
         path: '/initial_screen',
-        pageBuilder: (context, state) => const MaterialPage(
+        pageBuilder:
+            (context, state) =>
+                const MaterialPage(
           child: InitialScreen(),
         ),
       ),
+
+      // UPDATE
       GoRoute(
         path: '/update_screen',
-        pageBuilder: (context, state) => const MaterialPage(
+        pageBuilder:
+            (context, state) =>
+                const MaterialPage(
           fullscreenDialog: true,
           maintainState: true,
           child: UpdateDialog(),
@@ -101,4 +140,3 @@ class MyAppRouter {
     ],
   );
 }
-```
